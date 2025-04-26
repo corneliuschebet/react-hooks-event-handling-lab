0a1c4ae27091161878831ad9cc922889af93842f
Cornelius Chebet <cornelius.chebet@student.moringaschool.com>
2025-04-26 10:45:16 +0300

    working

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
-
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from "react";
+function Keypad() {
+  function handleChange(event) {
+    // console.log(event.target.value)
+    console.log("Entering password...");
+  }
+  return (
+    <div>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
